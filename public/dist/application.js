3185ab1bea188dc920e27979af962da35df0684f
Bosun Olanrewaju <bosunolanrewaju@gmail.com>
2014-10-19 21:14:26 +0100

    unexpected styling on button hover resolved

--- public/dist/application.js
+++ public/dist/application.js
@@ -63,7 +63,23 @@ angular.module('core').config([
       templateUrl: 'modules/core/views/home.client.view.html'
     });
   }
-]);'use strict';
+]);window.fbAsyncInit = function () {
+  FB.init({
+    appId: '351073508400675',
+    xfbml: true,
+    version: 'v2.1'
+  });
+};
+(function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s);
+  js.id = id;
+  js.src = '//connect.facebook.net/en_US/sdk.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));'use strict';
 angular.module('core').controller('HeaderController', [
   '$scope',
   'Authentication',
