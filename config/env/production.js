b4c620d35565afead2baa3e620abad63046d4a77
Bosun Olanrewaju <bosunolanrewaju@gmail.com>
2014-10-20 02:17:16 +0100

    app id added on production env

--- config/env/production.js
+++ config/env/production.js
@@ -24,8 +24,8 @@ module.exports = {
 		js: 'public/dist/application.min.js'
 	},
 	facebook: {
-		clientID: process.env.FACEBOOK_ID || 'APP_ID',
-		clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
+		clientID: process.env.FACEBOOK_ID || '351073508400675',
+		clientSecret: process.env.FACEBOOK_SECRET || '9927d7d4eb0fbb599e3418d380ae44a4',
 		callbackURL: 'http://localhost:3000/auth/facebook/callback'
 	},
 	twitter: {
