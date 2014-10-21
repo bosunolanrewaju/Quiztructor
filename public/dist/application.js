6305c712cfa6d50d3676536fa11d6d364d41db2e
Bosun Olanrewaju <bosunolanrewaju@gmail.com>
2014-10-21 18:38:59 +0100

    quiz id or quiz slug url added

--- public/dist/application.js
+++ public/dist/application.js
@@ -69,11 +69,7 @@ window.fbAsyncInit = function () {
     appId: '351073508400675',
     xfbml: true,
     version: 'v2.1'
-  });  // FB.login(function(){
-       //     FB.api('/me/feed', 'post', {message: 'Hey guys! I just posted a quiz on quiztructor online quiz app'});
-       //   }, {
-       //     scope: 'publish_actions'
-       //   });
+  });
 };
 (function (d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
--- public/modules/core/config/facebook.js
+++ public/modules/core/config/facebook.js
@@ -2,18 +2,12 @@
 
 
 window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '351073508400675',
-      xfbml      : true,
-      version    : 'v2.1'
-    });
-
-    // FB.login(function(){
-    //     FB.api('/me/feed', 'post', {message: 'Hey guys! I just posted a quiz on quiztructor online quiz app'});
-    //   }, {
-    //     scope: 'publish_actions'
-    //   });
-  };
+      FB.init({
+        appId      : '351073508400675',
+        xfbml      : true,
+        version    : 'v2.1'
+      });
+};
 
   (function(d, s, id){
      var js, fjs = d.getElementsByTagName(s)[0];
